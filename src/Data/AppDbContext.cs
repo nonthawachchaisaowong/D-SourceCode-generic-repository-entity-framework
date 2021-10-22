0709b7aebd283c6ba963e4ead1615c1942e2602d
Nonthawach Chaisaowong <60466913+nonthawachchaisaowong@users.noreply.github.com>
2021-10-22 10:07:56 +0700

    Remove unuse code

--- src/Data/AppDbContext.cs
+++ src/Data/AppDbContext.cs
@@ -1,12 +1,9 @@
-﻿using Core.Domain.Entities;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions options) : base(options) { }
-
-        public DbSet<Post> Posts { get; set; }
+        public AppDbContext(DbContextOptions options) : base(options) { }      
     }
 }
